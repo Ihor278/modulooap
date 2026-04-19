c3d51e18756cc556dec7fed96bb11b362548ae07
Ihor278 <121.24lutsiv.i@nltu.lviv.ua>
2026-04-19 18:09:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+interface IDocumentVisitor
+{
+    void Visit(Paragraph paragraph);
+}
+
+class RowStyleVisitor : IDocumentVisitor
+{
+    public void Visit(Paragraph paragraph)
+    {
+        Console.WriteLine($"[ROW] {paragraph.Text}");
+    }
+}
+
+class ColumnStyleVisitor : IDocumentVisitor
+{
+    public void Visit(Paragraph paragraph)
+    {
+        foreach (char c in paragraph.Text)
+        {
+            Console.WriteLine(c);
+        }
+        Console.WriteLine();
+    }
+}
+
+interface IDocumentElement
+{
+    void Accept(IDocumentVisitor visitor);
+}
+
+class Paragraph : IDocumentElement
+{
+    public string Text { get; set; }
+
+    public Paragraph(string text)
+    {
+        Text = text;
+    }
+
+    public void Accept(IDocumentVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
+
+class Document
+{
+    private List<IDocumentElement> elements = new List<IDocumentElement>();
+
+    public void Add(IDocumentElement element)
+    {
+        elements.Add(element);
+    }
+
+    public void ApplyVisitor(IDocumentVisitor visitor)
+    {
+        foreach (var element in elements)
+        {
+            element.Accept(visitor);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Document doc = new Document();
+
+        doc.Add(new Paragraph("Hello World"));
+        doc.Add(new Paragraph("Visitor Pattern Example"));
+
+        string choice = Console.ReadLine();
+
+        IDocumentVisitor visitor;
+
+        if (choice == "1")
+            visitor = new RowStyleVisitor();
+        else
+            visitor = new ColumnStyleVisitor();
+
+        doc.ApplyVisitor(visitor);
+    }
+}
